fc41de256b4ff3a43de3c0503dc23180f3577c4e
Flurin <f.schwerzmann@gmail.com>
2017-02-21 01:12:31 +0100

    Test test

---
+++ app/src/main/java/gagge/test/flurin/gitest/MainActivity.java
@@ -0,0 +1,14 @@
+package gagge.test.flurin.gitest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hwfieh
+    }
+}
